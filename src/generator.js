161b0c68855dce39888b039dc541b06182dfe56c
yuanchuan <yuanchuan23@gmail.com>
2017-05-19 15:00:59 +0800

    rewrite generator

--- src/generator.js
+++ src/generator.js
@@ -1,141 +1,126 @@
 import * as cond from './cond';
 import * as func from './function';
+import * as utils from './utils';
 
-function values(obj) {
-  return Array.isArray(obj)
-    ? obj : Object.keys(obj).map(k => obj[k]);
-}
-
-function with_args(fn, ...args) {
-  return args.reduce((f, arg) => f.apply(null, values(arg)), fn);
-}
-
-function join_line(arr) {
-  return (arr || []).join('\n');
-}
-
-function rule_set() {
-  const set = {};
-  const props = {};
-  const styles = {
-    host: '', cells: ''
-  };
-  return {
-    prop(name, value) {
-      props[name] = value;
-    },
-    add(selector, rule) {
-      let rules = set[selector];
-      if (!rules) rules = set[selector] = [];
-      rules.push.apply(
-        rules,
-        Array.isArray(rule) ? rule : [rule]
-      );
-    },
-    output() {
-      Object.keys(set).forEach(selector => {
-        let is_host = selector.startsWith(':host');
-        styles[is_host ? 'host': 'cells'] += `
-          ${ selector } {
-            ${ join_line(set[selector]) }
+class Rules {
+  constructor(tokens) {
+    this.tokens = tokens;
+    this.rules = {};
+    this.props = {};
+    this.styles = {
+      host: '', cells: ''
+    }
+  }
+  add_rule(selector, rule) {
+    let rules = this.rules[selector];
+    if (!rules) {
+      rules = this.rules[selector] = [];
+    }
+    rules.push.apply(rules, utils.make_array(rule));
+  }
+  compose_selector(count, psudo = '') {
+    return `.cell:nth-of-type(${ count }) .shape${ psudo }`;
+  }
+  compose_value(value, coords) {
+    return value.reduce((result, val) => {
+      switch (val.type) {
+        case 'text': {
+          result += val.value;
+          break;
+        }
+        case 'function': {
+          let fn = func[val.name.substr(1)];
+          if (fn) {
+            result += utils.apply_args(
+              fn, coords, val.arguments
+            );
           }
-        `;
-      });
-      return { props, styles }
+        }
+      }
+      return result;
+    }, '');
+  }
+  compose_rule(token, coords) {
+    let property = token.property;
+    let value = this.compose_value(token.value, coords);
+    if (property == 'transition') {
+      this.props.has_transition = true;
+    }
+    return `${ property }: ${ value };`;
+  }
+  compose(coords, tokens) {
+    (tokens || this.tokens).forEach((token, i) => {
+      if (token.skip) return false;
+      switch (token.type) {
+        case 'rule':
+          this.add_rule(
+            this.compose_selector(coords.count),
+            this.compose_rule(token, coords)
+          );
+          break;
+
+        case 'psudo': {
+          if (token.selector.startsWith(':doodle')) {
+            token.selector =
+              token.selector.replace(/^\:+doodle/, ':host');
+          }
+
+          let is_host_selector =
+            token.selector.startsWith(':host');
+
+          if (is_host_selector) {
+            token.skip = true;
+          }
+
+          let psudo_rules = token.styles.map(s =>
+            this.compose_rule(s, coords)
+          );
+
+          let selector = is_host_selector
+            ? token.selector
+            : this.compose_selector(coords.count, token.selector);
+
+          this.add_rule(selector, psudo_rules);
+          break;
+        }
+
+        case 'cond':
+          let fn = cond[token.name.substr(1)];
+          if (fn) {
+            let result = utils.apply_args(fn, coords, token.arguments);
+            if (result) {
+              this.compose(coords, token.styles);
+            }
+          }
+          break;
+      }
+    });
+  }
+
+  output() {
+    Object.keys(this.rules).forEach(selector => {
+      let is_host = selector.startsWith(':host');
+      const target = is_host ? 'host': 'cells';
+      this.styles[target] += `
+        ${ selector } {
+          ${ utils.join_line(this.rules[selector]) }
+        }
+      `;
+    });
+    return {
+      props: this.props,
+      styles: this.styles
     }
   }
 }
 
-function compose_selector(count, psudo = '') {
-  return `.cell:nth-of-type(${ count }) .shape${ psudo }`;
-}
-
-function compose_value(value, coords) {
-  return value.map(val => {
-    switch (val.type) {
-      case 'text': {
-        return val.value;
-      }
-      case 'function': {
-        let fn = func[val.name.substr(1)];
-        if (fn) {
-          return with_args(fn, coords, val.arguments);
-        }
-      }
-      default: return '';
-    }
-  }).join('');
-}
-
-function compose_rule(token, set, coords) {
-  let property = token.property;
-  let value = compose_value(token.value, coords);
-  if (property == 'transition') {
-    set.prop('has_transition', true);
-  }
-  return `${ property }: ${ value };`;
-}
-
-function compose_tokens(tokens, set, coords) {
-  tokens.forEach((token, i) => {
-    if (token.skip) return false;
-
-    switch (token.type) {
-      case 'rule':
-        set.add(
-          compose_selector(coords.count),
-          compose_rule(token, set, coords)
-        );
-        break;
-
-      case 'psudo': {
-        if (token.selector.startsWith(':doodle')) {
-          token.selector =
-            token.selector.replace(/^\:+doodle/, ':host');
-        }
-
-        let is_host_selector =
-          token.selector.startsWith(':host');
-
-        if (is_host_selector) {
-          token.skip = true;
-        }
-
-        let psudo_rules = token.styles.map(s =>
-          compose_rule(s, set, coords)
-        );
-
-        let selector = is_host_selector
-          ? token.selector
-          : compose_selector(coords.count, token.selector);
-
-        set.add(selector, psudo_rules);
-        break;
-      }
-
-      case 'cond':
-        let fn = cond[token.name.substr(1)];
-        if (fn) {
-          let result = with_args(fn, coords, token.arguments);
-          if (result) {
-            compose_tokens(token.styles, set, coords);
-          }
-        }
-        break;
-    }
-  });
-}
-
+export default
 function generator(tokens, size) {
-  let set = rule_set();
-  let count = 0;
-  for (let x = 1; x <= size.x; ++x) {
+  let rules = new Rules(tokens);
+  for (let x = 1, count = 0; x <= size.x; ++x) {
     for (let y = 1; y <= size.y; ++y) {
-      count++;
-      compose_tokens(tokens, set, { x, y, count});
+      rules.compose({ x, y, count: ++count});
     }
   }
-  return set.output();
+  return rules.output();
 }
-
-export default generator;
---
+++ src/utils.js
@@ -0,0 +1,18 @@
+export function values(obj) {
+  if (Array.isArray(obj)) return obj;
+  return Object.keys(obj).map(k => obj[k]);
+}
+
+export function apply_args(fn, ...args) {
+  return args.reduce((f, arg) =>
+    f.apply(null, values(arg)), fn
+  );
+}
+
+export function join_line(arr) {
+  return (arr || []).join('\n');
+}
+
+export function make_array(arr) {
+  return Array.isArray(arr) ? arr : [arr];
+}
