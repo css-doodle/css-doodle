468d7b0c5a44c7910bdc0080e6d4b2df4ff64bdb
yuanchuan <yuanchuan23@gmail.com>
2017-10-12 16:18:37 +0800

    docs: a new look

--- docs/main.js
+++ docs/main.js
@@ -1,8 +1,12 @@
 (function() {
 
-  if (!window.customElements || !document.head.attachShadow) {
-    document.body.className += 'oldie';
-  }
+  const nameLinks = document.querySelectorAll('a[name]');
+  [].forEach.call(nameLinks, link => {
+    let a = document.createElement('a');
+    a.innerHTML = '#';
+    a.href = '#' + link.name;
+    link.parentNode.insertBefore(a, link);
+  });
 
   function removeSpaces(input) {
     return input.replace(/[\s\n\t]/g, '');
@@ -42,55 +46,56 @@
   });
 
   const doodles = {
-    'leaves': indent(`
+    leaves: indent(`
       :doodle {
-        clip-path: @shape(circle);
-        background-color: #f6ffed;
+        @grid: 8 / 90%;
+        @shape: circle;
       }
 
-      transition: .2s ease @rand(.6s);
+      transition: .2s @rand(.6s);
+      border-radius: @pick(100% 0, 0 100%);
 
       will-change: transform;
       transform: scale(@rand(.25, 1.25));
 
-      border-radius: @pick(
-        100% 0, 0 100%
-      );
-
       background: hsla(
-        calc(5 * @index()), 70%, 60%,@rand(.8)
+        calc(120 + 3 * @index()), 70%, 68%,
+        @rand(.8)
       );
     `),
+    lines: indent(`
+			:doodle {
+				@grid: 7 / 100%;
+			}
 
-    star: indent(`
-      :doodle {
-        clip-path: @shape(bud, 5);
-        background: #673AB7;
-      }
+			@place-cell: center;
+			@size: calc(@index() * 1.55%);
 
-      clip-path: @shape(bud, 5);
+			transition: .2s ease;
+			transform: rotate(calc(@index() * 3deg));
 
-      background: hsla(
-        calc(300 + 3 * @index()),
-        75%, 70%, @rand(.8)
-      );
-
-      transition: .2s ease @rand(300ms);
-      transform:
-        scale(@rand(.2, 1.5))
-        translate3d(
-          @rand(-50%, 50%),
-          @rand(-50%, 50%),
-          0
-        );
+			border-radius: 30%;
+			border: 1px solid hsla(
+				calc(120 + 3 * @index()), 70%, 68%,
+				@rand(.8)
+			);
     `)
+  }
+
+ 	let config = {
+    value: doodles[Math.random() < .5 ? 'lines' : 'leaves'],
+    mode: 'css',
+    insertSoftTab: true,
+    theme: '3024-day',
+    matchBrackets: true,
+    smartIndent: true,
+    tabSize: 2
   }
 
   let doodle = document.createElement('css-doodle');
   doodle.title = 'Click to update';
-  doodle.grid = '7, 7';
   if (doodle.update) {
-    doodle.update(doodles.leaves);
+    doodle.update(config.value);
   }
 
   let playground = document.querySelector('.playground');
@@ -100,22 +105,10 @@
   container.appendChild(doodle);
   container.addEventListener('click', function(e) {
     e.preventDefault();
-    if (!toggled() || container.hasAttribute('front')) {
-      if (e.target.matches('css-doodle')) {
-        doodle.refresh();
-      }
-    }
+		if (e.target.matches('css-doodle')) {
+			doodle.update();
+		}
   });
-
-  let config = {
-    value: doodles.leaves,
-    mode: 'css',
-    insertSoftTab: true,
-    theme: '3024-day',
-    matchBrackets: true,
-    smartIndent: true,
-    tabSize: 2
-  }
 
   let editor = CodeMirror(source, config);
   let old = removeSpaces(editor.getValue());
@@ -125,6 +118,7 @@
     timer = setTimeout(function() {
       if (old != removeSpaces(editor.getValue())) {
         if (doodle.update) {
+          doodle.innerHTML = editor.getValue();
           doodle.update(editor.getValue());
         }
         old = removeSpaces(editor.getValue());
@@ -134,47 +128,7 @@
 
   document.querySelector('.docs').addEventListener('click', function(e) {
     if (e.target.matches('css-doodle.click-to-update')) {
-      e.target.refresh();
-    }
-  });
-
-  function toggled() {
-    let front = playground.querySelector('[front]');
-    return getComputedStyle(front).transform != 'none';
-  }
-
-  container.addEventListener('click', function() {
-    if (toggled()) {
-      source.removeAttribute('front');
-      container.setAttribute('front', '');
-    }
-  });
-
-  source.addEventListener('click', function() {
-    if (toggled()) {
-      let back = !source.hasAttribute('front');
-      container.removeAttribute('front');
-      source.setAttribute('front', '');
-      if (back) {
-        setTimeout(function() {
-          editor.setValue(editor.getValue().trimRight());
-          editor.refresh();
-        }, 200);
-      }
-    }
-  });
-
-  let nav = document.querySelector('.playground .nav');
-  nav.addEventListener('click', function(e) {
-    if (e.target.matches('li[data-name]')) {
-      let name = e.target.getAttribute('data-name');
-      nav.setAttribute('data-current', name);
-      let newStyle = doodles[name];
-      if (newStyle) {
-        doodle.style.display = 'none';
-        editor.setValue(newStyle);
-        doodle.style.display = '';
-      }
+      e.target.update();
     }
   });
 
@@ -196,8 +150,8 @@
         <div class="shape">
           <css-doodle>
             :doodle {
-              clip-path: @shape(${ name }, ${ param || 1});
-              background: rebeccapurple;
+              @shape: ${ name } ${ param || 1};
+              background: #60569e;
             }
           </css-doodle>
           <p class="title">
