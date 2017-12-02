016482c5e7bdc943873db53db47116874eb3eed7
yuanchuan <yuanchuan23@gmail.com>
2017-12-03 06:14:47 +0800

    docs: new code samples

--- docs/main.js
+++ docs/main.js
@@ -123,6 +123,24 @@
         from { transform: rotate(var(--f)) }
         to   { transform: rotate(var(--t)) }
       }
+    `),
+    dashed: indent(`
+      :doodle {
+        @grid: 1x10 / 85%;
+      }
+
+      @place-cell: center;
+      @size: calc(@index() * 10%);
+
+      border-radius: 50%;
+      border-style: dashed;
+      border-width: calc(@index() * 4px);
+      border-color: hsla(
+        calc(20 * @index()), 70%, 68%,
+        calc(3 / @index() * .8)
+      );
+
+      transform: rotate(@rand(360deg));
     `)
   }
 
