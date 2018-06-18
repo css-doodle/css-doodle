755c88669a9a3969ee047d87141bacf04991fa35
yuanchuan <yuanchuan23@gmail.com>
2018-06-18 13:53:35 +0800

    reformat

--- test/issues/issue-11.js
+++ test/issues/issue-11.js
@@ -5,11 +5,11 @@ const getValue = (value) => {
   let result = property['@place-cell'](value)
 
   return result
-          .match(reg)
-          .map((str) => {
-            return str.match(/\d+\% \* 2/)[0].replace(/(\d\%).*/, '$1')
-          })
-          .join(' ')
+    .match(reg)
+    .map((str) => {
+      return str.match(/\d+\% \* 2/)[0].replace(/(\d\%).*/, '$1')
+    })
+    .join(' ')
 }
 
 describe('issues', () => {
@@ -18,7 +18,7 @@ describe('issues', () => {
       let match = getValue('0')
       expect(match).toEqual('0% 0%')
     })
-    
+
     it(`should covert 'left top' to '0% 0%'`, () => {
       let match = getValue('left top')
       expect(match).toEqual('0% 0%')
@@ -28,7 +28,7 @@ describe('issues', () => {
       let match = getValue('right bottom')
       expect(match).toEqual('100% 100%')
     })
-    
+
     it(`should covert 'top right' to '100% 0%'`, () => {
       let match = getValue('top right')
       expect(match).toEqual('100% 0%')
