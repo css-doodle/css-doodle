5d468cae5f573313aedecf5caeb8af26ad0b963d
yuanchuan <yuanchuan23@gmail.com>
2021-05-16 11:41:21 +0800

    Simplify test

--- test/parse-grid.js
+++ test/parse-grid.js
@@ -1,50 +1,62 @@
-import test from 'ava';
+import ava from 'ava';
 import parseGrid from '../src/parser/parse-grid';
+
+function compare(fn) {
+  return t => {
+    fn((code, result, print) => {
+      let applied = parseGrid(code);
+      if (print) console.log(applied);
+      t.deepEqual(result, applied);
+    });
+  }
+}
+
+function test(name, fn) {
+  ava(name, compare(fn));
+}
+
+for (let m in ava) {
+  test[m] = (name, fn) => ava[m](name, compare(fn));
+}
 
 const defaultGrid = {
   x: 1, y: 1, z: 1, count: 1, ratio: 1
 };
 
-test.only('invalid values', t => {
-  t.deepEqual(parseGrid('random'), defaultGrid);
-  t.deepEqual(parseGrid(''), defaultGrid);
-  t.deepEqual(parseGrid('-1'), defaultGrid);
+test('invalid values', t => {
+  t('random', defaultGrid);
+  t('', defaultGrid);
+  t('-1', defaultGrid);
 });
 
-test.only('seperator', t => {
+test('seperator', t => {
 
-  t.deepEqual(parseGrid('1x1'), defaultGrid);
+  t('1x1', defaultGrid);
 
-  t.deepEqual(parseGrid('1X1'), defaultGrid);
+  t('1X1', defaultGrid);
 
-  t.deepEqual(parseGrid('1,1'), defaultGrid);
+  t('1,1', defaultGrid);
 
-  t.deepEqual(parseGrid('1，1'), defaultGrid);
+  t('1，1', defaultGrid);
 
-  t.deepEqual(parseGrid('1，   1'), defaultGrid);
+  t('1，   1', defaultGrid);
 
-  t.deepEqual(parseGrid('1 x 1'), defaultGrid);
+  t('1 x 1', defaultGrid);
 
 });
 
-test.only('clamp value', t => {
+test('clamp value', t => {
 
-  t.deepEqual(parseGrid('0'), defaultGrid);
+  t('0', defaultGrid);
 
-  t.deepEqual(parseGrid('0x1'), defaultGrid);
+  t('0x1', defaultGrid);
 
-  t.deepEqual(parseGrid('2000,1'), {
-    x: 1024, y: 1, z: 1, count: 1024, ratio: 1024
-  });
+  t('2000,1', { x: 1024, y: 1, z: 1, count: 1024, ratio: 1024 });
 
-  t.deepEqual(parseGrid('2000'), {
-    x: 32, y: 32, z: 1, count: 1024, ratio: 1
-  });
+  t('2000', { x: 32, y: 32, z: 1, count: 1024, ratio: 1 });
 
-  t.deepEqual(parseGrid('0.5'), defaultGrid);
+  t('0.5', defaultGrid);
 
-  t.deepEqual(parseGrid('1x5.2'), {
-    x: 1, y: 5, z: 1, count: 5, ratio: 1/5
-  });
+  t('1x5.2', { x: 1, y: 5, z: 1, count: 5, ratio: 1/5 });
 
 });
