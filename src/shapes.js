742ab4765de213e361f40932ba81e13b4554e31b
yuanchuan <yuanchuan23@gmail.com>
2021-10-29 21:28:31 +0800

    Fix `frame` calculation logic

--- src/shapes.js
+++ src/shapes.js
@@ -154,9 +154,9 @@ function create_polygon_points(option, fn) {
   }
 
   let split = option.split || 180;
-  let scale = option.scale || 1;
-  let turn = option.turn || 1;
-  let frame = option.frame;
+  let scale = Number(option.scale) || 1;
+  let turn = Number(option.turn) || 1;
+  let frame = Number(option.frame) || 1;
   let fill = option['fill'] || option['fill-rule'];
 
   let rad = (PI * 2) * turn / split;
@@ -186,8 +186,9 @@ function create_polygon_points(option, fn) {
 
   if (frame !== undefined) {
     add(first_point);
-    let w = (frame || 1) / 100 * turn;
-    if (w <= 0) w = 2 / 1000;
+    let w = frame / 100;
+    if (turn > 1) w *= 2;
+    if (w <= 0) w = .002;
     for (let i = 0; i < split; ++i) {
       let t = -rad * i;
       let [x, y, dx = 0, dy = 0] = fn(t, i);
