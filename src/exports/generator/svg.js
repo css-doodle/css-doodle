a548f2f907b248f4bcc63dc7b6f85ccb72867c15
yuanchuan <yuanchuan23@gmail.com>
2022-12-17 13:02:55 +0800

    Reduce imports of the exported svg function

--- src/exports/generator/svg.js
+++ src/exports/generator/svg.js
@@ -1,8 +1,6 @@
 import { generate_css } from '../../generator/css.js';
 import parse_css from './../../parser/parse-css.js';
 import parse_svg from './../../parser/parse-svg.js';
-import parse_grid from './../../parser/parse-grid.js';
-import { make_tag_function } from '../../utils/index.js';
 
 export default function svg(rules) {
   let parsedSvg = parse_svg(rules);
@@ -14,7 +12,7 @@ export default function svg(rules) {
   }
   let result = generate_css(
     parse_css(`${inlineVariable} background: @svg(${rules});`),
-    parse_grid('1x1')
+    { x: 1, y: 1, z: 1, count: 1, ratio: 1 }
   );
   let raw = result && result.styles && result.styles.cells || '';
   let cut = raw.substring(raw.indexOf('%3'), raw.lastIndexOf('");'));
