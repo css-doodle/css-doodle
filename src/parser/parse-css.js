e97a38c442bc3ea6da4689d1ec28cdb855025a57
yuanchuan <yuanchuan23@gmail.com>
2023-02-12 10:46:18 +0800

    Fix parsing function composition

--- src/parser/parse-css.js
+++ src/parser/parse-css.js
@@ -266,6 +266,7 @@ function read_arguments(it, composition, doodle) {
   while (!it.end()) {
     c = it.curr();
     let prev = it.curr(-1);
+    let next = it.curr(1);
     let start = it.index();
     if ((/[\('"`]/.test(c) && prev !== '\\')) {
       if (stack.length) {
@@ -328,7 +329,7 @@ function read_arguments(it, composition, doodle) {
       }
       arg += c;
     }
-    if (composition && (it.curr(1) == ')' || !/[0-9a-zA-Z_\-.]/.test(it.curr())) && !stack.length) {
+    if (composition && ((next == ')' || next == ';') || !/[0-9a-zA-Z_\-.]/.test(it.curr())) && !stack.length) {
       if (group.length) {
         args.push(normalize_argument(group));
       }
