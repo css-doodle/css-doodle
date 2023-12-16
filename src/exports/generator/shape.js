7b0cf9b3cb3b49fd521f5a8fe29beebdd0de304e
yuanchuan <yuanchuan23@gmail.com>
2023-12-16 23:55:48 +0800

    Update shape creation in export function

--- src/exports/generator/shape.js
+++ src/exports/generator/shape.js
@@ -1,21 +1,7 @@
-import parse_shape_commands from '../../parser/parse-shape-commands.js';
-import { shapes, create_shape_points } from '../../generator/shapes.js';
+import { create_shape } from '../../generator/shapes.js';
 
-export default function shape(type = '', ...args) {
-  type = String(type).trim();
-  let points = [];
-  if (type.length) {
-    if (typeof shapes[type] === 'function') {
-      points = shapes[type](args);
-    } else {
-      let commands = type;
-      let rest = args.join(',');
-      if (rest.length) {
-        commands = type + ',' + rest;
-      }
-      let config = parse_shape_commands(commands);
-      points = create_shape_points(config, {min: 3, max: 3600});
-    }
-  }
+export default function shape(...args) {
+  let commands = args.join(',');
+  let { points } = create_shape(commands, 3, 3600);
   return `polygon(${points.join(',')})`;
 }
